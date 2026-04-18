4b330dfc8474219d49889fbd35de9cf878045a91
cjbytheocean <cjrodenbush2024@gmail.com>
2026-04-18 03:40:25 -0400

    Game complete.

--- Lab Prototype 4/Assets/BallManager.cs
+++ Lab Prototype 4/Assets/BallManager.cs
@@ -1,20 +1,35 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BallManager : MonoBehaviour
 {
     public HingeJoint2D connection;
-
     public Transform post;
     public LineRenderer lr; 
+    public GameObject goal;
 
     public bool allowRestart = false;
+    public bool hitGoal = false;
+
+    [SerializeField] TextMeshProUGUI resultText;
+
+    Vector3[] spawnPositions = {
+        new Vector3(-6f, -4f, 0f),
+        new Vector3(-4f, -4f, 0f),
+        new Vector3(-2f, -4f, 0f),
+        new Vector3(2f, -4f, 0f),
+        new Vector3(4f, -4f, 0f),
+        new Vector3(6f, -4f, 0f)
+    };
 
     void Start()
     {
         allowRestart = false;
         lr = GetComponent<LineRenderer>();
+        SetGoalPosition();
     }
     
     void Update()
@@ -29,6 +44,8 @@ public class BallManager : MonoBehaviour
         {
             Destroy(lr);
             Destroy(connection);
+            resultText.text = $"";
+            StartCoroutine(SetText());
         }
 
         if (Input.GetKeyDown(KeyCode.R))
@@ -41,5 +58,35 @@ public class BallManager : MonoBehaviour
     void RestartTheGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+
+    IEnumerator SetText()
+    {
+        yield return new WaitForSeconds(2f);
+
+        if (hitGoal)
+        {
+            resultText.text = $"You Win! Press 'R' to Restart.";
+            allowRestart = true;
+        }
+        else
+        {
+            resultText.text = $"You Lose! Press 'R' to Retry."; 
+            allowRestart = true;
+        }
+    }
+
+    public void SetGoalPosition()
+    {
+        int rIndex = Random.Range(0, spawnPositions.Length);
+        Instantiate(goal, spawnPositions[rIndex], Quaternion.identity);
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.CompareTag("Goal"))
+        {
+            hitGoal = true;
+        }
     }
 }
